90f0b272a6665c48a619f61fa07c06a61aa20a04
muhsecha <muhammadsecha2@gmail.com>
2020-10-27 15:37:44 +0700

    add java

--- app/src/main/java/com/example/cafeorderapps/MainActivity.java
+++ app/src/main/java/com/example/cafeorderapps/MainActivity.java
@@ -2,13 +2,30 @@ package com.example.cafeorderapps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnManager,btnKasir,btnKoki,btnPelayan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnManager = findViewById(R.id.btn_manager);
+        btnKasir = findViewById(R.id.btn_kasir);
+        btnKoki = findViewById(R.id.btn_koki);
+        btnPelayan = findViewById(R.id.btn_pelayan);
+
+        btnPelayan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            }
+        });
     }
 }
