2e0a4ce913f0632f700466c452fb683d47b259a0
DeaAnandita <dea2018rplrus2@gmail.com>
2020-11-04 12:36:08 +0700

    bottomnav

--- app/src/main/java/com/example/cafeorderapps/MainActivity.java
+++ app/src/main/java/com/example/cafeorderapps/MainActivity.java
@@ -1,27 +1,24 @@
 package com.example.cafeorderapps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
 
-    Button btnManager,btnKasir,btnKoki,btnPelayan;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnManager = findViewById(R.id.btn_manager);
-        btnKasir = findViewById(R.id.btn_kasir);
-        btnKoki = findViewById(R.id.btn_koki);
-        btnPelayan = findViewById(R.id.btn_pelayan);
+        btnLogin = findViewById(R.id.btnLogin);
 
-        btnPelayan.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, HomeActivity.class));
