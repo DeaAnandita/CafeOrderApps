e34ad30c61d71cee358074a6f3b991d548279ddb
DeaAnandita <dea2018rplrus2@gmail.com>
2020-11-14 12:00:52 +0700

    layout update

--- app/src/main/java/com/example/cafeorderapps/CartFragment.java
+++ app/src/main/java/com/example/cafeorderapps/CartFragment.java
@@ -7,13 +7,42 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class CartFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    DetailAdapter detailAdapter;
+    ArrayList<DetailModel> DetailModels;
+    Toolbar toolbar;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.cart_fragment, container, false);
+        View view = inflater.inflate(R.layout.cart_fragment, container, false);
+
+//        toolbar = view.findViewById(R.id.toolbarDetail);
+//
+//        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+//        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Detail Pesanan");
+//        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recyclerView = view.findViewById(R.id.rvDetail);
+
+        DetailModels = new ArrayList<>();
+        DetailModels.add(new DetailModel("1", "Susu Caramel", "15.000,-"));
+        DetailModels.add(new DetailModel("2", "Susu Caramel", "15.000,-"));
+
+        detailAdapter = new DetailAdapter(DetailModels);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(detailAdapter);
+
+        return view;
     }
 }
